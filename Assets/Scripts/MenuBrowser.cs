0e65a3093550805745732992a0ea2c6730389968
Oliver Stanton <os349@student.aru.ac.uk>
2023-12-01 23:53:09 +0000

    Main menu functionality, settings, index + music

--- Assets/Scripts/MenuBrowser.cs
+++ Assets/Scripts/MenuBrowser.cs
@@ -5,6 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MenuBrowser : MonoBehaviour
 {
+    // Only needed for loading the main menu
+    [SerializeField] private GameObject mainMenu;
+    
+    // Only needed for loading the index menu
+    [SerializeField] private GameObject indexMenu;
+    
+    // Only needed for loading the settings menu
+    [SerializeField] private GameObject settingsMenu;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +34,31 @@ public class MenuBrowser : MonoBehaviour
     {
         //SceneManager.LoadScene("Endless Mode");
         SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LoadIndexMenu()
+    {
+        // Get Main Menu game object, set it to inactive
+        transform.parent.parent.gameObject.SetActive(false);
+        indexMenu.SetActive(true);
+    }
+
+    public void LoadMainMenu()
+    {
+        // Get whatever parent menu and deactivate it
+        transform.parent.gameObject.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void LoadSettingsMenu()
+    {
+        // Get the Main Menu and deactivate it
+        transform.parent.parent.gameObject.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
